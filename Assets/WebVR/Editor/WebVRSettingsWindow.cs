bb3014b0db058d45c5e8a1bd22d79b044373063d
Kyle Reczek <kyle@banyango.com>
2018-06-19 21:38:42 -0600

    WebVR Settings
    + Added an editor window that will pop up when the plugin gets imported. It should only pop up if VR isn’t enabled and it will recommend that the user enables VR in their project.
    + Might be a good idea to have this be able to be triggered from the menu incase it gets closed.

---
+++ Assets/WebVR/Editor/WebVRSettingsWindow.cs
@@ -0,0 +1,51 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace WebVR.Editor
+{
+    [InitializeOnLoad]
+    public class WebVRSettingsWindow : EditorWindow
+    {
+        static WebVRSettingsWindow window;
+
+        static WebVRSettingsWindow()
+        {
+            EditorApplication.update += Update;
+        }
+
+        static void Update()
+        {
+            
+            if (!PlayerSettings.virtualRealitySupported)
+            {
+                window = GetWindow<WebVRSettingsWindow>(true);                               
+            
+                window.minSize = new Vector2(320, 440);
+                window.title = "WebVR Settings";
+            }
+            
+            EditorApplication.update -= Update;
+        }
+
+        private void OnGUI()
+        {
+            if(PlayerSettings.virtualRealitySupported)
+            {
+                GUILayout.Label("PlayerSettings.VRSupported is enabled");
+            }
+ 
+            
+            EditorGUILayout.HelpBox("Recommended project settings for WebVR:", MessageType.Warning);
+
+            if (!PlayerSettings.virtualRealitySupported)                   
+            {
+                if (GUILayout.Button("Enable PlayerSettings.VRSupported to test in Editor"))
+                {
+                    PlayerSettings.virtualRealitySupported = true;
+                    
+                    Debug.Log("Virtual Reality Enabled");
+                }
+            }
+        }
+    }
+}
